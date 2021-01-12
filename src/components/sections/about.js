c474757172d413321507ba667685019574364a62
ArthurFDLR <arthfind@gmail.com>
2021-01-12 11:33:45 -0600

    Minor

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -146,7 +146,7 @@ const About = () => {
             <p>
               I enjoy creating things that live on the internet, whether that be websites,
               applications, or anything in between. My goal is to always build products that provide
-              pixel-perfect, performant experiences.
+              pixel-perfect, performant experiences..
             </p>
 
             <p>
